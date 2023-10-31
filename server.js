53b93470440f4f1daf7dff7b88eb22a54549b373
brayann lima <brayannrlima100@gmail.com>
2023-10-31 20:29:02 -0300

    layout2 index rainha victoria

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.set('layout', './layout')
+app.set('layout', './layout2')
 
 
 app.use(express.static("public/"))
